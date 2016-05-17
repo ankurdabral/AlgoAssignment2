8ef7e869f33a693ab6b994b27aeb17d2fbec1fe2
ankurdabral <ankurdabral@gmail.com>
2016-05-17 16:57:36 +1000

    test

--- mazeAssign/mazeGenerator/ModifiedPrimsGenerator.java
+++ mazeAssign/mazeGenerator/ModifiedPrimsGenerator.java
@@ -4,6 +4,11 @@ import maze.Maze;
 
 public class ModifiedPrimsGenerator implements MazeGenerator {
 
+	private Maze maze;
+	public ModifiedPrimsGenerator(Maze maze) {
+		this.maze = maze;
+	}
+
 	@Override
 	public void generateMaze(Maze maze) {
 		// TODO Auto-generated method stub
